4a3e56b9e46a2e928bbcf67974eca8d09757a4e8
Yunfei Zhao <yunfei.zhao@ubisoft.com>
2023-02-14 19:13:35 +0100

    Pandas

--- algoexpert/find_closest_value_in_bst.py
+++ algoexpert/find_closest_value_in_bst.py
@@ -1,3 +1,4 @@
+from queue import Queue
 
 class Node():
     def __init__(self, value, left=None, right=None):
@@ -6,28 +7,46 @@ class Node():
         self.right = right
 
 
-def array_to_tree(array):
+def array_to_tree_dfs(array):
     n = len(array)
     if n>0:
-        root = Node(array[0].value)
+        root = Node(array[0])
 
     def dfs(node, index):
         # if index >= n:
         #     return
         # else:
-        if 2*(index+1) -1 <= n:
-            node.left = array[2*(index+1) -1]
+        if 2*(index+1) -1 < n and array[2*(index+1) -1] is not None:
+            node.left = Node(array[2*(index+1) -1])
             dfs(node.left, 2*(index+1) -1)
-        if 2*(index+1) <= n:
-            node.right = array[2*(index+1)]
+        if 2*(index+1) < n and array[2*(index+1)] is not None:
+            node.right = Node(array[2*(index+1)])
             dfs(node.right, 2*(index+1))
         return
     dfs(root, 0)
     return root
 
 
-def print_tree_bfs(root):
-    
+def tree_to_array_bfs(root):
+    q = Queue(maxsize = 0) # queue with infinity size
+    q.put(root)
+    array = []
+    def bfs():
+        if q.empty():
+            return
+        else:
+            node = q.get()
+            array.append(node.value)
+            if node.left != None:
+                q.put(node.left)
+            if node.right != None:
+                q.put(node.right)
+            bfs()
+        return
+    bfs()
+    return array
+
+            
 
 
 def findClosestValueInBst(tree, target):
@@ -35,14 +54,16 @@ def findClosestValueInBst(tree, target):
     value = tree.value
     
     def dfs(node):
+        nonlocal distance, value
         # stop condition
+        if(node is None):
+            return value
         if(node.value == target):
             return target
-        if(abs(node.value - target) > distance or (node.left==None and node.right==None)):
-            return value
 
-        value = node.value
-        distance = abs(node.value - target)
+        if abs(node.value - target) < distance:
+            value = node.value
+            distance = abs(value - target)
 
         # recursion part
         if(node.value > target):
@@ -55,6 +76,9 @@ def findClosestValueInBst(tree, target):
 
 if __name__ == '__main__':
     array = [5,3,10,2,4,8] + [None]*6 + [9] + [None]*2
-    root = array_to_tree(array)
+    root = array_to_tree_dfs(array)
+    new_array = tree_to_array_bfs(root) 
+    print(new_array)
+    print(findClosestValueInBst(root, 6))
     
 
---
+++ math/29_divide_two_integers.py
@@ -0,0 +1,12 @@
+import typing
+
+def solution(N):
+    pass
+
+def digit_sum(num):
+    return sum(int(digit) for digit in str(num))
+
+    
+
+
+if __name__ == '__main__':
